1d945bedd42eca50d5468f4ed35099f2fe1f6a2b
Revathi Kannan <37206283+RevathiKannanGithub@users.noreply.github.com>
2023-08-28 19:06:00 +0530

    Create isPalidrome.py

---
+++ isPalidrome.py
@@ -0,0 +1,9 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        sign = ''
+        if x < 0:
+            sign = '-'
+        reverse = sign.join(str(x)[::-1])
+        if str(x) == reverse:
+            return True
+        return False
