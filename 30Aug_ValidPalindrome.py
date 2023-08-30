a7f97076483d1552aa6b155c241d1588a0f5d3f2
Revathi Kannan <37206283+RevathiKannanGithub@users.noreply.github.com>
2023-08-30 20:16:11 +0530

    Update 30Aug_ValidPalindrome.py

--- 30Aug_ValidPalindrome.py
+++ 30Aug_ValidPalindrome.py
@@ -22,6 +22,7 @@
 # Output: true
 # Explanation: s is an empty string "" after removing non-alphanumeric characters.
 # Since an empty string reads the same forward and backward, it is a palindrome.
+#
 class Solution:
     def isPalindrome(self, s: str) -> bool:
         s = [i for i in s.lower() if i.isalnum()]
