3a23ea63325dd368946b58a07754b819efcd62e8
Revathi Kannan <37206283+RevathiKannanGithub@users.noreply.github.com>
2023-09-04 14:05:30 +0530

    Update 04Sep_NoOfSegmentsString.py

--- 04Sep_NoOfSegmentsString.py
+++ 04Sep_NoOfSegmentsString.py
@@ -1,6 +1,10 @@
 #
 # https://leetcode.com/problems/number-of-segments-in-a-string/description/
 #
+#####################################################################################################################################################
+#                                                            PROBLEM DESCRIPTION                                                                    #
+######################################################################################################################################################
+#
 # Given a string s, return the number of segments in the string.
 # A segment is defined to be a contiguous sequence of non-space characters.
 #
